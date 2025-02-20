2638cb50debb759f75920203a55de1d5e45557cd
albertprofe <albertprofe@gmail.com>
2025-02-20 19:08:03 +0100

    Initial commit

---
+++ src/main/java/com/example/userBorrowBook/UserBorrowBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.userBorrowBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserBorrowBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserBorrowBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/userBorrowBook/UserBorrowBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userBorrowBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBorrowBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
